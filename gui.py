c0945085167d188d8f8a7f34a4f0a466e00a4d88
vincentler <99483072+vincentler@users.noreply.github.com>
2022-03-12 12:08:29 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,16 +1,26 @@
 # coordinate system:
 # x axis: letters A-G
 # y axis: numbers 1-6
-from multiprocessing import Event
 import tkinter as tk
 from tkinter import ttk
+from multiprocessing import Event
 SIZE = 39.5
 
 root = tk.Tk()
 root.geometry('280x290')
 #root.resizable(False, False)
-root.title('12 gewinnt!')
 
+def showxy(event):
+    '''
+    show x, y coordinates of mouse click position
+    event.x, event.y relative to ulc of widget (here root) 
+    '''
+    # xy relative to ulc of root
+    #xy = 'root x=%s  y=%s' % (event.x, event.y)
+    # optional xy relative to blue rectangle
+    xy = 'rectangle x=%s  y=%s' % (event.x-x1, event.y-y1)
+    root.title(xy)
+	
 title = tk.Label(root, text="12 gewinnt!")
 title.pack(ipadx = 40, ipady = 15)
 
@@ -31,7 +41,7 @@ for x in range(7):
         print(x2)
         y2 = y1 + SIZE
         print(y2)
-        canvas.create_rectangle((x1, y1, x2, y2), fill=color)
+        canvas.create_rectangle((x1, y1, x2, y2), fill=color, tag='rectangle')
 		
 		
 def a(event):
@@ -78,5 +88,25 @@ def getorigin(eventorigin):
 #message = tk.Label(root)
 #message.config(text='\u25C9 \u25CE')
 #message.pack()
+
+# create a canvas for drawing
+#w = 400
+#h = 400
+#canvas = tk.Canvas(root, width=w, height=h, bg='white')
+#canvas.pack()
+
+# draw a blue rectangle shape with 
+# upper left corner coordinates x1, y1
+# lower right corner coordinates x2, y2
+#x1 = 20
+#y1 = 30
+#x2 = 380
+#y2 = 370
+#cv.create_rectangle(x1, y1, x2, y2, fill="blue", tag='rectangle')
+
+# bind left mouse click within shape rectangle
+canvas.tag_bind('rectangle', '<Button-1>', showxy)
+
+root.mainloop()
 
 root.mainloop()
